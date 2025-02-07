dce346ad039556a1a327aca2d91d7352a83ead6a
melalj <mohammedelalj@outlook.com>
2025-02-07 17:16:18 +0100

    js make me angyyy >:(

--- back-end/Handlers/routes.go
+++ back-end/Handlers/routes.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -34,7 +35,40 @@ func Routes() *http.ServeMux {
 	mux.HandleFunc("/front-end/", func(w http.ResponseWriter, r *http.Request) {
 		FileHandler(w, r)
 	})
+	mux.HandleFunc("POST /api/login", AuthLogin)
 	return mux
+}
+
+func AuthLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "application/json" {
+		ErrorPagehandler(w, http.StatusUnsupportedMediaType, "Unsupported media type")
+		return
+	}
+	if r.Body == nil {
+		ErrorPagehandler(w, http.StatusBadRequest, "Request body is empty")
+		return
+	}
+	// Decode body JSON object
+	defer r.Body.Close()
+	var loginData loginData
+	err := json.NewDecoder(r.Body).Decode(&loginData)
+	if err != nil {
+		ErrorPagehandler(w, http.StatusBadRequest, "Invalid JSON data")
+		return
+	}
+	// TODO Implement authentication logic here
+	// if err := validateInput(loginData); err != nil {
+	// 	ErrorPagehandler(w, http.StatusBadRequest, err.Error())
+	// 	return
+	// }
+	// For demonstration purposes, let's assume successful authentication
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "no logic Logged in successfully"})
+}
+
+type loginData struct {
+	Username string `json:"name_or_email"`
+	Password string `json:"password"`
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
--- front-end/scripts/log_reg.js
+++ front-end/scripts/log_reg.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function goToRegistera(e) {
-        console.log(e,'azer')
+        console.log(e, 'azer')
         e.preventDefault();
         showLoginForm();
     };
@@ -53,3 +53,39 @@ document.addEventListener("DOMContentLoaded", () => {
     //     showLoginForm();
     // });
 });
+
+function fetching(e, target) {
+    e.preventDefault();
+    let data = {};
+    for (const element of e.target) {
+        if (element.value != '') {
+            console.log(element.value, element.id, element);
+            data[element.id] = element.value;
+        }
+    }
+    console.log(e.target, e.this, data, target);
+    let a = fetch(target, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error(' Network error \n\tstatus code :'+ response.status);
+        }
+        return response.json();
+    }).catch((error) => {
+            console.log('Error:', error);
+            // Return error object with a message
+            return { 'error': error.message };
+    });
+    a.then((result) => {
+        console.log('Response as object:', result);
+        for (let key in result) {
+            if (result.hasOwnProperty(key)) {
+                alert(`${key}: ${result[key]}`);
+            }
+        }
+    });
+}
